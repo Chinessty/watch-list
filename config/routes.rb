e50f9884acd8a718746fe66654047370d24af0d4
edwina gamer <gamer.edwina@gmail.com>
2021-12-14 10:58:13 -0500

    Prepare rails app with external specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :lists, only: [:create, :index, :destroy]
-  get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new"
-  get "lists/:id", to: "lists#show", as: :list
-  post "lists", to: "lists#create"
-  get "lists/:id/edit", to: "lists#edit"
-  patch "lists/:id", to: "lists#update"
-  get "lists/:id", to: " list#destroy"
 end
